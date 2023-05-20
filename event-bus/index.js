80b6fdf2099c0205345fa2e9543f8f7a14564284
Zaid Qureshi <zaid.qureshi@lmsin.com>
2023-05-20 10:11:44 +0530

    till the query service implemeneted with react

--- event-bus/index.js
+++ event-bus/index.js
@@ -5,14 +5,14 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
-app.post('/events', (req, res) => {
-    const event = req.body;
-    axios.post('http://localhost:4000/events', event);
-    axios.post('http://localhost:4001/events', event);
-    axios.post('http://localhost:4002/events', event);
-    res.send({ status: "OK" });
+app.post("/events", (req, res) => {
+  const event = req.body;
+  axios.post("http://localhost:4000/events", event); // Post Service
+  axios.post("http://localhost:4001/events", event); // Comment Service
+  axios.post("http://localhost:4002/events", event); // Query Service
+  res.send({ status: "OK" });
 });
 
 app.listen(4005, () => {
-    console.log("Server listening to port 4005");
+  console.log("Server listening to port 4005");
 });
---
+++ react-app/src/commentList.js
@@ -0,0 +1,9 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default ({ comments }) => {
+  const renderedComments = comments.map((comment) => {
+    return <li key={comment.id}>{comment.content}</li>;
+  });
+  return <ul>{renderedComments}</ul>;
+};
